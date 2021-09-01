2f054d263fc5bbb0ddbb752efa9f09937df09585
HoPGoldy <hpgless@outlook.com>
2021-09-01 13:52:57 +0800

    init case

---
+++ src/Foo.vue
@@ -0,0 +1,21 @@
+<template>
+<!-- The default value in the template is correct -->
+default prop fetchData: {{defaultFunc}}
+</template>
+
+<script setup lang="ts">
+import { defaultFunc } from './service';
+import type { DefaultFunc } from './service';
+
+interface Props {
+  fetchData?: DefaultFunc
+}
+
+// set default function prop
+const props = withDefaults(defineProps<Props>(), {
+  fetchData: defaultFunc
+});
+
+// Why it's the return value, not the function itself here?
+console.log('======> default prop fetchData:', props.fetchData);
+</script>
---
+++ src/service.ts
@@ -0,0 +1,5 @@
+export const defaultFunc = function(): string {
+    return 'result'
+}
+
+export type DefaultFunc = () => string
